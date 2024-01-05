51f4ad207a4117b44280054f47941ad13a99e969
Huy Vu <huy.vu@skedulo.com>
2024-01-05 11:45:01 +0700

    [ENG-43642] Add minuteInterval

--- src/proxies/components/models/index.ts
+++ src/proxies/components/models/index.ts
@@ -121,6 +121,7 @@ export type DatetimeEditorViewProps = {
     hasError?: boolean
     timezone?: TimeZoneType
     mode: "date" | "time" | "datetime"
+    minuteInterval?: 1 | 2 | 3 | 4 | 5 | 6 | 10 | 12 | 15 | 20 | 30
     onValueChanged(newValue: string):void
 }
 
